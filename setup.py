3f70ad8485f978920d4e580dfa2c05c9d92a4cfb
giovane <giovaneliberato@gmail.com>
2014-02-13 18:25:47 -0200

    Changing path o setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(author='Jeffrey Scudder',
+      author_email='j.s@google.com',
+      license='Apache 2.0',
+      url='https://github.com/qmagico/google-atom',
+      packages=['atom'],
+      version='2.0.18',
+      package_dir = {'atom':'atom'},)
